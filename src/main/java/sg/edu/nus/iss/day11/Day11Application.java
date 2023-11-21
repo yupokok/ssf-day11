e664f2ed791f2e016c66101d85614a65d13ef6c1
yupokok <kimberlywsp@gmail.com>
2023-11-21 14:20:54 +0800

    Added error message and manually changed port

--- src/main/java/sg/edu/nus/iss/day11/Day11Application.java
+++ src/main/java/sg/edu/nus/iss/day11/Day11Application.java
@@ -1,5 +1,9 @@
 package sg.edu.nus.iss.day11;
 
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day11Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day11Application.class, args);
+		// SpringApplication.run(Day11Application.class, args);
+
+		SpringApplication app = new SpringApplication(Day11Application.class);
+		String port = "8080";
+		ApplicationArguments argOptions = new DefaultApplicationArguments(args);
+
+		if (argOptions.containsOption("port")) {
+			port = argOptions.getOptionValues("port").get(0);
+		}
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+		
 		System.out.println("Hi! This is my first springboot application started...");
 	}
 
